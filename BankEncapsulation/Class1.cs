f69c521fa71b00a95fe8caa20aa4628ab44b7111
jared mckemy <jaredmkcemy4@gmail.com>
2023-09-06 11:57:52 -0700

    atm amounts

---
+++ BankEncapsulation/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 150;
+
+        public void Deposit(double adding)
+        {
+                _balance += adding;
+        }
+        public double GetBalance()
+        {
+                return _balance;
+        }
+        public void Withdraw(double taking)
+            {
+                _balance -= taking;
+            }
+        }
+    }
+    
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            bool isInAtm = false;
+            while (!isInAtm)
+            {
+                Console.WriteLine("press 0 to see balance: press 1 to deposit: press 2 to withdraw");
+                int chose = int.Parse(Console.ReadLine());
+                if (chose == 0)
+                {
+
+                    Console.WriteLine($"You have {account.GetBalance()}");
+                }
+                if (chose == 1)
+                {
+                    Console.WriteLine("how much will you deposit");
+                    double addingNum = double.Parse(Console.ReadLine());
+                    account.Deposit(addingNum);
+                }
+                if (chose == 2)
+                {
+                    Console.WriteLine("how much will you withdraw");
+                    double subtractNum = double.Parse(Console.ReadLine());
+                    account.Withdraw(subtractNum);
+                }
+                if (chose == 3)
+                {
+                    isInAtm = true;
+                }
+            }
+            
         }
     }
 }
+
